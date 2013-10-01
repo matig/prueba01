4b02d4190c7c2132e28d6d6b348667e963fa9d9a
Matias <Matias@Andromeda>
2013-10-01 18:55:23 -0300

    asd

--- web/src/web.java
+++ web/src/web.java
@@ -1,9 +1,7 @@
 
 public class web {
     public static void main(String[] parametro) {
-        System.out.println("Hola Mundo Java")
-        
-        ;
+        System.out.println("Hola Mundo Java");
     }
 
 }
